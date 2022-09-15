7ebf891cd2388227f91e4e83ab6d5f539c3f9cfa
Vladzag <ru.vlad.zag@gmail.com>
2022-09-15 19:26:30 +0300

    Добавлен первый, простейший тест для мобильника. Почти работающий.

---
+++ src/test/java/AppiumWebTest.java
@@ -0,0 +1,42 @@
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileCommand;
+import io.appium.java_client.ios.IOSDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class AppiumWebTest {
+
+    @Test
+    public void someTestName() throws MalformedURLException {
+        URL serverURL = new URL("http://0.0.0.0:4723/wd/hub");
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "iOS");
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "15.5");
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "iPhone 12");
+        capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "Appium");
+        capabilities.setCapability(MobileCapabilityType.BROWSER_NAME, "Safari");
+        capabilities.setCapability(MobileCapabilityType.FULL_RESET, "true");
+        capabilities.setCapability("showXcodeLog", "true");
+        capabilities.setCapability("autoAcceptAlerts", "true");
+
+        AppiumDriver driver = new IOSDriver(serverURL, capabilities);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://www.wikipedia.org/");
+        driver.findElement(By.id("searchInput")).sendKeys("meadow", Keys.ENTER);
+        Assertions.assertTrue(driver.getCurrentUrl().equals("https://en.wikipedia.org/wiki/Meadow"));
+
+
+
+        driver.quit();
+    }
+}
