1a48d13aeadc052dea28733cf8c73b848d573989
Vladzag <ru.vlad.zag@gmail.com>
2022-09-17 22:13:38 +0300

    Рост и развитие: исправлены некоторые ошибки, добавлены тесты на клик для смены языка

--- src/test/java/AppiumWebTest.java
+++ src/test/java/AppiumWebTest.java
@@ -1,14 +1,11 @@
 import io.appium.java_client.AppiumDriver;
-import io.appium.java_client.MobileCommand;
 import io.appium.java_client.ios.IOSDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.concurrent.TimeUnit;
 
@@ -18,12 +15,12 @@ import java.net.URL;
 public class AppiumWebTest {
 
     @Test
-    public void someTestName() throws MalformedURLException, InterruptedException {
+    public void openWikiSearch() throws MalformedURLException, InterruptedException {
         URL serverURL = new URL("http://0.0.0.0:4723/wd/hub");
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "iOS");
         capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "15.5");
-        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "iPhone 12");
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "iPhone 8");
         capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "Appium");
         capabilities.setCapability(MobileCapabilityType.BROWSER_NAME, "Safari");
         capabilities.setCapability(MobileCapabilityType.FULL_RESET, "true");
@@ -31,13 +28,193 @@ public class AppiumWebTest {
         capabilities.setCapability("autoAcceptAlerts", "true");
 
         AppiumDriver driver = new IOSDriver(serverURL, capabilities);
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         driver.get("https://www.wikipedia.org/");
         driver.findElement(By.id("searchInput")).sendKeys("meadow");
         WebElement someElement = driver.findElement(new By.ByCssSelector("#search-form > fieldset > button"));
         someElement.click();
-        Thread.sleep(3000);
-        Assertions.assertTrue(driver.getCurrentUrl().equals("https://en.wikipedia.org/wiki/Meadow"));
+        Assertions.assertTrue(driver.getCurrentUrl().equals("https://en.m.wikipedia.org/wiki/Meadow"));
         driver.quit();
+    }
+
+    @Test
+    public void switchLanguageFrench() throws MalformedURLException, InterruptedException {
+        URL serverURL = new URL("http://0.0.0.0:4723/wd/hub");
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "iOS");
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "15.5");
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "iPhone 8");
+        capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "Appium");
+        capabilities.setCapability(MobileCapabilityType.BROWSER_NAME, "Safari");
+        capabilities.setCapability(MobileCapabilityType.FULL_RESET, "true");
+        capabilities.setCapability("showXcodeLog", "true");
+        capabilities.setCapability("autoAcceptAlerts", "true");
+
+        AppiumDriver driver = new IOSDriver(serverURL, capabilities);
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.get("https://www.wikipedia.org/");
+        driver.findElement(By.id("js-link-box-fr")).click();
+        Thread.sleep(3000);
+        Assertions.assertTrue(driver.getCurrentUrl().equals("https://fr.m.wikipedia.org/wiki/Wikip%C3%A9dia:Accueil_principal"));
+    }
+    @Test
+    public void switchLanguageRussian() throws MalformedURLException, InterruptedException {
+        URL serverURL = new URL("http://0.0.0.0:4723/wd/hub");
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "iOS");
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "15.5");
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "iPhone 8");
+        capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "Appium");
+        capabilities.setCapability(MobileCapabilityType.BROWSER_NAME, "Safari");
+        capabilities.setCapability(MobileCapabilityType.FULL_RESET, "true");
+        capabilities.setCapability("showXcodeLog", "true");
+        capabilities.setCapability("autoAcceptAlerts", "true");
+
+        AppiumDriver driver = new IOSDriver(serverURL, capabilities);
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.get("https://www.wikipedia.org/");
+        driver.findElement(By.id("js-link-box-ru")).click();        Thread.sleep(3000);
+
+        Assertions.assertTrue(driver.getCurrentUrl().equals("https://ru.m.wikipedia.org/wiki/%D0%97%D0%B0%D0%B3%D0%BB%D0%B0%D0%B2%D0%BD%D0%B0%D1%8F_%D1%81%D1%82%D1%80%D0%B0%D0%BD%D0%B8%D1%86%D0%B0"));
+    }
+    @Test
+    public void switchLanguageJapanese() throws MalformedURLException, InterruptedException {
+        URL serverURL = new URL("http://0.0.0.0:4723/wd/hub");
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "iOS");
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "15.5");
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "iPhone 8");
+        capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "Appium");
+        capabilities.setCapability(MobileCapabilityType.BROWSER_NAME, "Safari");
+        capabilities.setCapability(MobileCapabilityType.FULL_RESET, "true");
+        capabilities.setCapability("showXcodeLog", "true");
+        capabilities.setCapability("autoAcceptAlerts", "true");
+
+        AppiumDriver driver = new IOSDriver(serverURL, capabilities);
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.get("https://www.wikipedia.org/");
+        driver.findElement(By.id("js-link-box-ja")).click();        Thread.sleep(3000);
+
+        Assertions.assertTrue(driver.getCurrentUrl().equals("https://ja.m.wikipedia.org/wiki/%E3%83%A1%E3%82%A4%E3%83%B3%E3%83%9A%E3%83%BC%E3%82%B8"));
+    }
+    @Test
+    public void switchLanguageSpanish() throws MalformedURLException, InterruptedException {
+        URL serverURL = new URL("http://0.0.0.0:4723/wd/hub");
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "iOS");
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "15.5");
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "iPhone 8");
+        capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "Appium");
+        capabilities.setCapability(MobileCapabilityType.BROWSER_NAME, "Safari");
+        capabilities.setCapability(MobileCapabilityType.FULL_RESET, "true");
+        capabilities.setCapability("showXcodeLog", "true");
+        capabilities.setCapability("autoAcceptAlerts", "true");
+
+        AppiumDriver driver = new IOSDriver(serverURL, capabilities);
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.get("https://www.wikipedia.org/");
+        driver.findElement(By.id("js-link-box-es")).click();        Thread.sleep(3000);
+        System.out.println(driver.getCurrentUrl());
+        Assertions.assertTrue(driver.getCurrentUrl().equals("https://es.m.wikipedia.org/wiki/Wikipedia:Portada"));
+    }
+    @Test
+    public void switchLanguageGerman() throws MalformedURLException, InterruptedException {
+        URL serverURL = new URL("http://0.0.0.0:4723/wd/hub");
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "iOS");
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "15.5");
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "iPhone 8");
+        capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "Appium");
+        capabilities.setCapability(MobileCapabilityType.BROWSER_NAME, "Safari");
+        capabilities.setCapability(MobileCapabilityType.FULL_RESET, "true");
+        capabilities.setCapability("showXcodeLog", "true");
+        capabilities.setCapability("autoAcceptAlerts", "true");
+
+        AppiumDriver driver = new IOSDriver(serverURL, capabilities);
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.get("https://www.wikipedia.org/");
+        driver.findElement(By.id("js-link-box-de")).click();        Thread.sleep(3000);
+
+        Assertions.assertTrue(driver.getCurrentUrl().equals("https://de.m.wikipedia.org/wiki/Wikipedia:Hauptseite"));
+    }
+    @Test
+    public void switchLanguageItaliano() throws MalformedURLException, InterruptedException {
+        URL serverURL = new URL("http://0.0.0.0:4723/wd/hub");
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "iOS");
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "15.5");
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "iPhone 8");
+        capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "Appium");
+        capabilities.setCapability(MobileCapabilityType.BROWSER_NAME, "Safari");
+        capabilities.setCapability(MobileCapabilityType.FULL_RESET, "true");
+        capabilities.setCapability("showXcodeLog", "true");
+        capabilities.setCapability("autoAcceptAlerts", "true");
+
+        AppiumDriver driver = new IOSDriver(serverURL, capabilities);
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.get("https://www.wikipedia.org/");
+        driver.findElement(By.id("js-link-box-it")).click();        Thread.sleep(3000);
+
+        Assertions.assertTrue(driver.getCurrentUrl().equals("https://it.m.wikipedia.org/wiki/Pagina_principale"));
+    }
+    @Test
+    public void switchLanguageChinese() throws MalformedURLException, InterruptedException {
+        URL serverURL = new URL("http://0.0.0.0:4723/wd/hub");
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "iOS");
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "15.5");
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "iPhone 8");
+        capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "Appium");
+        capabilities.setCapability(MobileCapabilityType.BROWSER_NAME, "Safari");
+        capabilities.setCapability(MobileCapabilityType.FULL_RESET, "true");
+        capabilities.setCapability("showXcodeLog", "true");
+        capabilities.setCapability("autoAcceptAlerts", "true");
+
+        AppiumDriver driver = new IOSDriver(serverURL, capabilities);
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.get("https://www.wikipedia.org/");
+        driver.findElement(By.id("js-link-box-zh")).click();
+        Thread.sleep(3000);
+        Assertions.assertTrue(driver.getCurrentUrl().equals("https://zh.m.wikipedia.org/wiki/Wikipedia:%E9%A6%96%E9%A1%B5"));
+    }
+    @Test
+    public void switchLanguagePortuguese() throws MalformedURLException, InterruptedException {
+        URL serverURL = new URL("http://0.0.0.0:4723/wd/hub");
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "iOS");
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "15.5");
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "iPhone 8");
+        capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "Appium");
+        capabilities.setCapability(MobileCapabilityType.BROWSER_NAME, "Safari");
+        capabilities.setCapability(MobileCapabilityType.FULL_RESET, "true");
+        capabilities.setCapability("showXcodeLog", "true");
+        capabilities.setCapability("autoAcceptAlerts", "true");
+
+        AppiumDriver driver = new IOSDriver(serverURL, capabilities);
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.get("https://www.wikipedia.org/");
+        driver.findElement(By.id("js-link-box-pt")).click();        Thread.sleep(3000);
+
+        Assertions.assertTrue(driver.getCurrentUrl().equals("https://pt.m.wikipedia.org/wiki/Wikip%C3%A9dia:P%C3%A1gina_principal"));
+    }
+    @Test
+    public void switchLanguageArabic() throws MalformedURLException, InterruptedException {
+        URL serverURL = new URL("http://0.0.0.0:4723/wd/hub");
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "iOS");
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "15.5");
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "iPhone 8");
+        capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "Appium");
+        capabilities.setCapability(MobileCapabilityType.BROWSER_NAME, "Safari");
+        capabilities.setCapability(MobileCapabilityType.FULL_RESET, "true");
+        capabilities.setCapability("showXcodeLog", "true");
+        capabilities.setCapability("autoAcceptAlerts", "true");
+
+        AppiumDriver driver = new IOSDriver(serverURL, capabilities);
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.get("https://www.wikipedia.org/");
+        driver.findElement(By.id("js-link-box-ar")).click();        Thread.sleep(3000);
+
+        Assertions.assertTrue(driver.getCurrentUrl().equals("https://ar.m.wikipedia.org/wiki/%D8%A7%D9%84%D8%B5%D9%81%D8%AD%D8%A9_%D8%A7%D9%84%D8%B1%D8%A6%D9%8A%D8%B3%D9%8A%D8%A9"));
     }
 }
